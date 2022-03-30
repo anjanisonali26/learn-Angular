4b15477467663866c1f64837b3dd424cd1e04652
anjanisonali26 <anjanibendre260201@gmail.com>
2022-03-30 23:12:31 +0700

    update homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule
