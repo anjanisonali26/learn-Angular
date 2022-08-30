8a94e9b19b4f7a7af9d04490c9f62560c9795750
anjanisonali26 <anjanibendre260201@gmail.com>
2022-08-30 21:14:57 +0700

    commit feature web marketplace

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        'ui-pink-8000': '#DF416B',
+      }
+    },
+
+  },
+  plugins: [
+  ],
+}
